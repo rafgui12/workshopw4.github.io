5fe5103c2341e8167f878953768a9453ab280ceb
Rafael Angulo <rafgui12@gmail.com>
2021-12-18 14:00:56 -0500

    part 3 task 3

--- matchingGame.js
+++ matchingGame.js
@@ -9,22 +9,36 @@ function generateFaces() {
 
     for(let i = 0; i<numberOfFaces; i++) {
 
-        var face = document.createElement("img");
+        var face = document.createElement("img"); // Image
+
+        let randomTop = Math.floor(Math.random()*400)+1 // Random TOP
+        let randomLeft = Math.floor(Math.random()*400)+1 // Random LEFT
+
+        // Properties For Faces
         face.src = "img/smile.png"
-        let randomTop = Math.floor(Math.random()*400)+1
-        let randomLeft = Math.floor(Math.random()*400)+1
         face.style.top = randomTop + "px"
         face.style.left =  randomLeft + "px"
         face.addEventListener('click', nextLevel)
+
         theLeftSide.appendChild(face);
-        const leftSideImages = theLeftSide.cloneNode(true).lastChild;
+        const leftSideImages = theLeftSide.cloneNode(true);
+        leftSideImages.removeChild(leftSideImages.lastChild);
         theRightSide.appendChild(leftSideImages);
     }
 
-    function nextLevel() {
-        event.stopPropagation();
-        numberOfFaces += 5;
-        generateFaces();
-    }
+    document.body.addEventListener('click', gameOver) // Call GameOver
 
+}
+
+function nextLevel() {
+    event.stopPropagation();
+    numberOfFaces += 5;
+    generateFaces();
+}
+
+
+function gameOver(){
+    alert('Game Over')
+    document.body.removeEventListener('click', gameOver)
+    theLeftSide.removeEventListener('click', gameOver)
 }
