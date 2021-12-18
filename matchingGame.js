5bd5624ec2c090c28da9619a90b6c6cb88e807a9
Rafael Angulo <rafgui12@gmail.com>
2021-12-18 11:37:24 -0500

    part 1 and Part 2 for Task 2

--- matchingGame.js
+++ matchingGame.js
@@ -1,2 +1,19 @@
 let numberOfFaces = 5;
 const theLeftSide = document.getElementById('leftSide');
+
+console.log("Random Number",Math.floor(Math.random()*400)+1)
+
+function generateFaces() {
+
+    for(let i = 0; i<numberOfFaces; i++) {
+
+        var face = document.createElement("img");
+        face.src = "img/smile.png"
+        let randomNumb = Math.floor(Math.random()*400)+1
+        face.style.top = randomNumb + "px"
+        face.style.left =  randomNumb + "px"
+        theLeftSide.appendChild(face);
+
+    }
+
+}
